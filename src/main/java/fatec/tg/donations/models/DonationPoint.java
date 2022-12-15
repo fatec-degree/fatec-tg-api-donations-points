d6cbcc272e9cd034905755fa259c5a79538ddab3
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2022-12-14 23:01:34 -0300

    feat: add model DonationPoint

---
+++ src/main/java/fatec/tg/donations/models/DonationPoint.java
@@ -0,0 +1,28 @@
+package fatec.tg.donations.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter @Setter
+@Table(name = "tb_addresses")
+@Entity
+public class DonationPoint {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String cep;
+    private String state;
+    private String city;
+    private String district;
+    private String street;
+    private String number;
+    private Double lng;
+    private Double lat;
+    @Column(name = "creation_date")
+    private LocalDateTime creationDate;
+
+}
