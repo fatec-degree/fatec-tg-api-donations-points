432ee5f06efb51935288f419bea175c334c8cad2
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2022-12-14 22:21:30 -0300

    init project

---
+++ src/main/java/fatec/tg/donations/DonationsApplication.java
@@ -0,0 +1,13 @@
+package fatec.tg.donations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fatec/tg/donations/DonationsApplicationTests.java
@@ -0,0 +1,13 @@
+package fatec.tg.donations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
