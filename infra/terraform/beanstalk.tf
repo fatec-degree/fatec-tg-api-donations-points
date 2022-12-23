adee7ca7cad8bcb64dd688dce939e7e80c508298
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2022-12-22 23:47:38 -0300

    infra: fix terraform

--- infra/terraform/beanstalk.tf
+++ infra/terraform/beanstalk.tf
@@ -3,17 +3,9 @@ resource "aws_elastic_beanstalk_application" "api_donations_beanstalk" {
   description = "API for FATEC's TG"
 }
 
-resource "aws_elastic_beanstalk_application_version" "api_donations_version" {
-  name        = "api-donations-1.0"
-  application = aws_elastic_beanstalk_application.api_donations_beanstalk.name
-  bucket      = aws_s3_bucket.api_donations_s3.id
-  key         = aws_s3_object.api_donations_jar.id
-}
-
 resource "aws_elastic_beanstalk_environment" "api_donations_prd" {
   name                = "api-donations-prd"
   application         = aws_elastic_beanstalk_application.api_donations_beanstalk.name
-  version_label       = aws_elastic_beanstalk_application_version.api_donations_version.name
   solution_stack_name = "64bit Amazon Linux 2 v3.4.2 running Corretto 17"
   depends_on = [
     aws_db_instance.api_donations_rds
--- infra/terraform/s3.tf
+++ infra/terraform/s3.tf
@@ -6,12 +6,6 @@ resource "aws_s3_bucket" "api_donations_s3" {
   }
 }
 
-resource "aws_s3_object" "api_donations_jar" {
-  bucket = aws_s3_bucket.api_donations_s3.id
-  key    = "api-donations/donations-0.0.1-SNAPSHOT.jar"
-  source = "../../target/donations-0.0.1-SNAPSHOT.jar"
-}
-
 resource "aws_s3_bucket_acl" "github_actions_acl" {
   bucket = aws_s3_bucket.api_donations_s3.id
   acl    = "private"
