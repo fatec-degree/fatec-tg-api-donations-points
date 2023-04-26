3bdf95b50e629e8983619f5c1aee368a373293b4
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2023-04-26 20:35:57 -0300

    infra: fix depends_on

--- deploy/terraform/s3.tf
+++ deploy/terraform/s3.tf
@@ -20,4 +20,7 @@ resource "aws_s3_bucket_ownership_controls" "ownership_controls_s3" {
 resource "aws_s3_bucket_acl" "github_actions_acl" {
   bucket = aws_s3_bucket.api_donations_s3.id
   acl    = "private"
+  depends_on = [
+    aws_s3_bucket_ownership_controls.ownership_controls_s3
+  ]
 }
