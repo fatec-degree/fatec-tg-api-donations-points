4541fc53339b84aba643b57aef8328948cb9e4bd
Pedro Henrique Pereira Almeida <pedro.6571almeida@gmail.com>
2023-04-26 20:28:53 -0300

    infra: fix s3 bucket ACL
    https://aws.amazon.com/pt/blogs/aws/heads-up-amazon-s3-security-changes-are-coming-in-april-of-2023/

--- deploy/terraform/s3.tf
+++ deploy/terraform/s3.tf
@@ -9,6 +9,14 @@ resource "aws_s3_bucket" "api_donations_s3" {
   }
 }
 
+resource "aws_s3_bucket_ownership_controls" "ownership_controls_s3" {
+  bucket = aws_s3_bucket.api_donations_s3.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
 resource "aws_s3_bucket_acl" "github_actions_acl" {
   bucket = aws_s3_bucket.api_donations_s3.id
   acl    = "private"
